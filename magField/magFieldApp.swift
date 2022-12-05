59f8ae5ae0596d146027bbdd4605a76b25e0e33d
James Lambert <james.a.lambert@gmail.com>
2022-12-05 00:29:08 +0000

    Initial Commit

---
+++ magField/magFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  magFieldApp.swift
+//  magField
+//
+//  Created by J Lambert on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct magFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
